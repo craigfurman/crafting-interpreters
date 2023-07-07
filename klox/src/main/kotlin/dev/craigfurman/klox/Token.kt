43d607e81d8ce8386f67eaf406537985d3a2ea89
Craig Furman <craig.furman89@gmail.com>
2023-07-07 11:48:25 +0100

    Combine token files

--- klox/src/main/kotlin/dev/craigfurman/klox/Token.kt
+++ klox/src/main/kotlin/dev/craigfurman/klox/Token.kt
@@ -1,8 +1,34 @@
 package dev.craigfurman.klox
 
+import dev.craigfurman.klox.TokenType.*
+
 data class Token(
     val type: TokenType,
     val lexeme: String,
     val literal: Any?,
     val line: Int,
 )
+
+enum class TokenType {
+    // Single-character tokens
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // One or two character tokens
+    BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+
+    // Literals
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
+
+val keywords = arrayOf(
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+).associateBy { it.name.lowercase() }
